3e19cc6e25920d3baaa4001b53875613fa5aceeb
B1910018 <tuyenb1910018@student.ctu.edu.vn>
2022-10-08 14:36:40 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
-const routes = [{
+const routes = [
+    {
     path: "/",
     name: "contactbook",
     component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+        
 ];
 
 const router = createRouter({
